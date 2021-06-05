f96769fb740b881fe8e54539a1523b6f7da870a1
joshfeds14 <joshfeds14@gmail,com>
2021-06-04 23:51:03 -0400

    Exercise 10

---
+++ src/main/java/oop/exercise/Exercise10.java
@@ -0,0 +1,13 @@
+package oop.exercise;
+import java.util.Scanner;
+public class Exercise10
+{
+    static Scanner input = new Scanner(System.in);
+
+    public static void main( String[] args) {
+        for(int i = 0; i  < 2; i++)
+        {
+            System.out.print("Hello!");
+        }
+    }
+}
